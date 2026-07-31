2e7e91ff00ff493db8bc94f4f0883e3b96c9143c
Chukwuma Peter Chinedu <donshangti2@gmail.com>
2026-07-31 03:41:16 +0100

    Add version information and log message to chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,7 @@
+// ==========================================
+// Kulzzy Radio Live Community
+// Chat Engine
+// Version 2.5.0
+// ==========================================
 
+console.log("Chat Engine Loaded");
