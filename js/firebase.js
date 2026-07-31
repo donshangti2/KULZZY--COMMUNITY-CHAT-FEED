724cf4e29c99a2de39ac2967a2ef1afa78f7b881
Chukwuma Peter Chinedu <donshangti2@gmail.com>
2026-07-31 03:47:46 +0100

    Update Firebase configuration and imports

--- js/firebase.js
+++ js/firebase.js
@@ -4,10 +4,45 @@
 // Version 2.5.0
 // ==========================================
 
-// Firebase configuration will go here
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Firebase initialization will go here
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  update,
+  get,
+  query,
+  limitToLast,
+  onValue,
+  onChildAdded
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Database references will go here
+const firebaseConfig = {
+  apiKey: "AIzaSyBH85NGWsSAK5cubbPdnmunwYZFZpj_CB0",
+  authDomain: "kulzzy-radio-chat.firebaseapp.com",
+  databaseURL: "https://kulzzy-radio-chat-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kulzzy-radio-chat",
+  storageBucket: "kulzzy-radio-chat.firebasestorage.app",
+  messagingSenderId: "510100635134",
+  appId: "1:510100635134:web:9d3b2e983eb6e9a385d4af"
+};
 
-// Export everything for the app
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+// Export everything
+export {
+  db,
+  ref,
+  push,
+  set,
+  update,
+  get,
+  query,
+  limitToLast,
+  onValue,
+  onChildAdded
+};
