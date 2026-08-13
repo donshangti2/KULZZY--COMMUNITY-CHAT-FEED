ce293afb4c4c5ebbeb91a6c040cd0833f3e647cc
Chukwuma Peter Chinedu <donshangti2@gmail.com>
2026-08-14 00:23:50 +0100

    Refactor firebase.js to include additional imports

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,8 @@
+// ==========================================
+// Kulzzy Radio Live Community
+// firebase.js
+// ==========================================
+
 import {
     initializeApp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
@@ -9,20 +14,19 @@ import {
     set,
     update,
     get,
-    onChildAdded,
     onValue,
+    onChildAdded,
     query,
-    limitToLast
+    limitToLast,
+    onDisconnect,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-import {
-    getStorage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBH85NGWsSAK5cubbPdnmunwYZFZpj_CB0",
+    apiKey:
+        "AIzaSyBH85NGWsSAK5cubbPdnmunwYZFZpj_CB0",
 
     authDomain:
         "kulzzy-radio-chat.firebaseapp.com",
@@ -41,15 +45,15 @@ const firebaseConfig = {
 
     appId:
         "1:510100635134:web:9d3b2e983eb6e9a385d4af"
-
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
 
-const db = getDatabase(app);
 
-const storage = getStorage(app);
+const db =
+    getDatabase(app);
 
 
 export {
@@ -57,8 +61,6 @@ export {
     app,
 
     db,
-
-    storage,
 
     ref,
     push,
@@ -70,6 +72,9 @@ export {
     limitToLast,
 
     onValue,
-    onChildAdded
+    onChildAdded,
+
+    onDisconnect,
+    serverTimestamp
 
 };
